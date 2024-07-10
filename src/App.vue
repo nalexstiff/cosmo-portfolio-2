7ac87004e4c07f03399459f281f5ef1e2563f73c
Nathan Alexander Stiff <nalexstiff@gmail.com>
2024-07-10 15:14:37 -0400

    draft index page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SiteHeader from './components/SiteHeader.vue'
+import PageSection from './components/PageSection.vue'
+import Tile from './components/Tile.vue'
+import SiteFooter from './components/SiteFooter.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <SiteHeader />
+    <!-- splash image goes here -->
+    <PageSection>
+        <p><em>Hello! My name is Alex Stiff.</em></p>
+        <p>I'm an artist and designer in the Washington, D.C. area. I do animation background design and illustration, as well as video production, motion graphics and editing.</p>
+        <p>Have a look around my site, or get in touch via email at nalexstiff@gmail.com.</p>
+    </PageSection>
+    <PageSection>
+        <Tile>backgrounds</Tile>
+        <Tile>video</Tile>
+    </PageSection>
+    <SiteFooter />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
