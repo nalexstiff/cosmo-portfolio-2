784f367c64cc06676c6443b27c3815a5efb69143
Nathan Alexander Stiff <nalexstiff@gmail.com>
2024-07-11 14:07:16 -0400

    create a footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,13 @@
 <template>
-
+    <div class="footer">
+    </div>
 </template>
 
 <style scoped>
-
+.footer {
+    width: 100%;
+    height: 200px;
+    background-color: #b0b0b0;
+    /*background-image: linear-gradient(#e0e0e0, #c0c0c0, #808080);*/
+}
 </style>
