19bc7c169d1bfa9708923c60bf998d757348834e
Nathan Alexander Stiff <nalexstiff@gmail.com>
2024-07-11 13:48:45 -0400

    create page section component

--- src/components/PageSection.vue
+++ src/components/PageSection.vue
@@ -1,7 +1,22 @@
 <template>
-
+    <div class="section">
+        <div class="section-content">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.section {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e0e0e0;
+}
 
+.section-content {
+    width: 840px;
+    margin: 80px;
+}
 </style>
