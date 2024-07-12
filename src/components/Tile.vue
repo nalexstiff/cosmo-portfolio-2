dee1f96af2adc325868becc80352821f9fe7f789
Nathan Alexander Stiff <nalexstiff@gmail.com>
2024-07-11 20:11:56 -0400

    created tile component

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,7 +1,34 @@
-<template>
+<script setup>
+import { computed } from "vue"
 
+const props = defineProps(["imgSrc", "linkUrl"])
+
+const bgImg = computed(() => {
+    return 'url("' + props["imgSrc"] + '")'
+})
+</script>
+
+<template>
+    <div class="tile" :style="{ backgroundImage: bgImg }">
+        <h2 class="tile-text">
+            <slot></slot>
+        </h2>
+    </div>
 </template>
 
 <style scoped>
+.tile {
+    margin: 20px 0 20px 0;
+    width: 840px;
+    height: 560px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.tile-text {
+    color: #3f3f3f;
+    font-size: 72pt;
+}
 
 </style>
