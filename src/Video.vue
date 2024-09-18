17c96c26baf7504dde94aaa426a45f6f7ccc869e
Nathan Alexander Stiff <nalexstiff@gmail.com>
2024-09-17 22:01:05 -0400

    added video page placeholder

---
+++ src/Video.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SiteHeader from "./components/SiteHeader.vue"
+import PageSection from "./components/PageSection.vue"
+import SiteFooter from "./components/SiteFooter.vue"
+
+</script>
+
+<template>
+    <SiteHeader />
+    <PageSection>
+        <h1>Page under construction!</h1>
+        <p> Thank you for visiting! </p>
+    </PageSection>
+    <SiteFooter />
+</template>
---
+++ src/video.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import Video from './Video.vue'
+
+createApp(Video).mount('#app')
