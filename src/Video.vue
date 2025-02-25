3339d3513473029cde460e6212fa06e793f63dfd
Nathan Alexander Stiff <nalexstiff@gmail.com>
2025-02-25 15:26:49 -0500

    populate video page

--- src/Video.vue
+++ src/Video.vue
@@ -7,9 +7,37 @@ import SiteFooter from "./components/SiteFooter.vue"
 
 <template>
     <SiteHeader />
-    <PageSection>
-        <h1>Page under construction!</h1>
-        <p> Thank you for visiting! </p>
+    <PageSection content-width="1000px" content-margin="80px">
+        <h1>Animation</h1>
+        <p> See my hand-drawn, 3D and motion graphics animation </p>
+        <div class="embed-container" height="56.25%">
+            <iframe class="embed-content" src="https://www.youtube.com/embed/D6zpOQccGP0?si=1FbByWqFlwGgFhRG"></iframe>
+        </div>
     </PageSection>
     <SiteFooter />
 </template>
+
+<style>
+.embed-container {
+    width: 100%;
+    margin-bottom: 60px;
+    position: relative;
+    overflow: hidden;
+}
+
+.embed-container::after {
+    display: block;
+    content: "";
+    padding-top: 56.25%;
+}
+
+.embed-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+}
+
+</style>
